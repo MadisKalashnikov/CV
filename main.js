8417031448155277bc13cbd922ffa18e9a666ae1
Madis <kalashnikov.madis@gmail.com>
2023-03-25 18:01:46 +0200

    Ajax page load feature

---
+++ main.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const rootPath = window.location.origin
+	const navItems = document.querySelectorAll(".primary-nav .link")
+	const bodyEl = document.querySelector("body")
+	
+
+	const removeActiveState = () => {
+		navItems.forEach(item => item.classList.remove("active"))
+	}
+
+	const changePageHandler = (event) => {
+		event.preventDefault()
+
+		const pageToLoad = event.target.getAttribute("href")
+		const currentPage = window.location.href
+		console.log(`current page ${currentPage}, page to load ${pageToLoad}`)
+		bodyEl.querySelector(".main").classList.add("is-animating")
+		removeActiveState()
+		event.target.classList.add("active")
+
+		window.setTimeout(() => {
+			changePage(pageToLoad)
+		}, 300)
+	}
+
+	const changePage = async (page) => {
+		const newPageUrl = rootPath + page
+		const response = await fetch(newPageUrl)
+		const parser = new DOMParser()
+		const newDocument = parser.parseFromString(await response.text(), 'text/html')
+
+		const newContent = newDocument.querySelector(".main")
+
+		bodyEl.querySelector(".main").replaceWith(newContent)
+		bodyEl.querySelector(".main").classList.remove("is-animating")
+		const newUrl = new URL(newPageUrl)
+		window.history.pushState({}, "", newUrl)
+	}
+
+	navItems.forEach(item => item.addEventListener("click", changePageHandler))
+})
